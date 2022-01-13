4126741ec0aea11dfd7699973c104bdac4e71932
Shiven31 <96771819+Shiven31@users.noreply.github.com>
2022-01-14 00:33:44 +0530

    Add files via upload

---
+++ output.py
@@ -0,0 +1,31 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+===== RESTART: C:/Users/dell/Desktop/Shiven Python Assignment 1 (Output).py ====
+#Question Number 1
+Input your First Number= 56
+Input your Second Number= 46
+Input your Third Number= 16
+The Average of your Three Numbers =  39.333333333333336
+#Question Number 2
+Input your Gross Income= 123456789
+Input the number of Dependents= 6
+#Question Number 3
+Enter Your SID:  21104017
+Enter Your Name:  Shiven Goyal
+Enter Your CGPA:  10
+Enter Your Gender:  M
+Enter Your Course name:  Introduction to Computing
+Student [21104017, 'Shiven Goyal', 10.0, 'M', 'Introduction to Computing']
+Income Tax you have to pay =  24685757.8
+#Question Number 4
+Marks of The 1st student : 66
+Marks of The 2nd student : 96
+Marks of The 3rd student : 88
+Marks of The 4th student : 75
+Marks of The 5th student : 84
+[66, 75, 84, 88, 96]
+#Question Number 5(A)
+Colors ['Red', 'Green', 'White', 'Pink', 'Yellow']
+#Question Number 5(B)
+Colors ['Red', 'Green', 'White', 'Yellow', 'purple']
